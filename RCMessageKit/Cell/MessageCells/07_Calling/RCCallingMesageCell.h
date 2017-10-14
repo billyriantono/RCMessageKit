9e8db1a78b567049f69b7be5571e1fc64ecec109
ZeLiang <zeliang.shao@gmail.com>
2017-10-14 17:05:25 +0800

    Add calling custom cell

---
+++ RCMessageKit/Cell/MessageCells/07_Calling/RCCallingMesageCell.h
@@ -0,0 +1,13 @@
+//
+//  RCCallingMesageCell.h
+//  Elephant
+//
+//  Created by Zeliang Shao on 14/10/2017.
+//  Copyright © 2017 Shaw. All rights reserved.
+//
+
+#import <RCMessageKit/RCMessageKit.h>
+
+@interface RCCallingMesageCell : RCMessageCell
+
+@end
---
+++ RCMessageKit/Cell/MessageCells/07_Calling/RCCallingMesageCell.m
@@ -0,0 +1,24 @@
+//
+//  RCCallingMesageCell.m
+//  Elephant
+//
+//  Created by Zeliang Shao on 14/10/2017.
+//  Copyright © 2017 Shaw. All rights reserved.
+//
+
+#import "RCCallingMesageCell.h"
+
+@implementation RCCallingMesageCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
